670ef15f7d5733faf2d40d65fd03c6c3d5985a20
david <david.gozlan@epitech.eu>
2023-12-08 11:30:10 +0100

    feat - added complete send

---
+++ button.py
@@ -0,0 +1,78 @@
+from discord import Embed, ui, ButtonStyle
+from discord.ext import commands
+import discord
+
+import constant
+from constant import g_channels, g_roles
+
+class SendButton(ui.Button):
+    def __init__(self, level, label, channel_type: constant.ChannelType, feature=False):
+        super().__init__(style=ButtonStyle.success, label=label, emoji='👍')
+        self.level = level
+        self.feature = feature
+        self.channel_type = channel_type
+        self.channel_to_send = constant.ChannelType.MODERATOR if channel_type == constant.ChannelType.HELPER else constant.ChannelType.ANNOUNCEMENT
+
+    async def callback(self, inter: discord.Interaction):
+        mention = inter.user.mention if self.channel_type != constant.ChannelType.ANNOUNCEMENT else None
+            
+        if await request_message(self.level, self.channel_to_send, mention=mention, feature=self.feature):
+            return await inter.response.send_message(self.channel_type + " channel not defined")
+
+        self.disabled = True
+        embed = embed_level(self.level, 0x00ff00)
+        embed.add_field(name="Sent for ", value=("feature" if self.feature else "rate"), inline=False)
+        embed.add_field(name="Sent by", value=inter.user.mention, inline=False)
+
+        await inter.response.edit_message(embed=embed, view=None)
+                     
+
+class RejectButton(ui.Button):
+    def __init__(self, level, label):
+        super().__init__(style=ButtonStyle.danger, label=label)
+        self.level = level
+
+    async def callback(self, inter: discord.Interaction):
+        await inter.response.send_message('Button clicked!')
+
+
+
+def embed_level(level: constant.Level, color=0xff0000):
+    embed = Embed(title="Level Title", description="This is level description", color=color)
+    embed.add_field(name="ID", value=level.id, inline=False)
+    embed.add_field(name="Video", value=level.video, inline=False)
+
+    # embed.set_author(name=level.user, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", icon_url=level.user.avatar_url)
+    #embed.set_thumbnail(url=level.user.avatar_url)
+    #embed.set_image(url=level.user.avatar_url)
+
+    return embed
+
+
+async def request_message(level: constant.Level, channel_type: constant.ChannelType, mention=None, feature=None):
+    channel = g_channels.get_channel(channel_type)
+    if channel is None:
+        return True
+    
+    if channel_type == constant.ChannelType.ANNOUNCEMENT:
+        embed = embed_level(level, 0x00ff00)
+    else:
+        embed = embed_level(level)
+    if mention is not None:
+        embed.add_field(name="Previous sent by", value=mention, inline=False)
+    if feature is not None:
+        embed.add_field(name="Sent for ", value=("feature" if feature else "rate"), inline=False)
+    
+    view = ui.View()
+
+    
+    if channel_type != constant.ChannelType.ANNOUNCEMENT:
+        button_send = SendButton(level, 'Send', channel_type)
+        button_send_feature = SendButton(level, 'Send feature', channel_type, True)
+        button_cancel = RejectButton(level, 'Reject')
+
+        view.add_item(button_send)
+        view.add_item(button_send_feature)
+        view.add_item(button_cancel)
+
+    await channel.send(embed=embed, view=view)
--- constant.py
+++ constant.py
@@ -15,7 +15,7 @@ class ChannelType(Enum):
     ANNOUNCEMENT = 2
 
 class Level:
-    def __init__(self, id, description, video, user, status):
+    def __init__(self, id, description, video, user, status: LevelStatus = LevelStatus.PENDING):
         self.id = id
         self.description = description
         self.video = video
@@ -28,3 +28,59 @@ class LevelRequest:
         self.description = description
         self.video = video
         self.user = user
+
+class Channels:
+    def __init__(self, helper_channel, moderator_channel, announcement_channel, log_channel):
+        self.helper_channel = helper_channel
+        self.moderator_channel = moderator_channel
+        self.announcement_channel = announcement_channel
+        self.log_channel = log_channel
+
+    def get_channel(self, channel_type: ChannelType):
+        if channel_type == ChannelType.HELPER:
+            return self.helper_channel
+        elif channel_type == ChannelType.MODERATOR:
+            return self.moderator_channel
+        elif channel_type == ChannelType.ANNOUNCEMENT:
+            return self.announcement_channel
+        elif channel_type == ChannelType.LOG:
+            return self.log_channel
+        else:
+            return None
+        
+    def set_channel(self, channel_type: ChannelType, channel):
+        if channel_type == ChannelType.HELPER:
+            self.helper_channel = channel
+        elif channel_type == ChannelType.MODERATOR:
+            self.moderator_channel = channel
+        elif channel_type == ChannelType.ANNOUNCEMENT:
+            self.announcement_channel = channel
+        elif channel_type == ChannelType.LOG:
+            self.log_channel = channel
+        else:
+            return True
+        
+class Roles:
+    def __init__(self, helper_role, moderator_role):
+        self.helper_role = helper_role
+        self.moderator_role = moderator_role
+
+    def get_role(self, role_type: RoleType):
+        if role_type == RoleType.HELPER:
+            return self.helper_role
+        elif role_type == RoleType.MODERATOR:
+            return self.moderator_role
+        else:
+            return None
+    
+    def set_role(self, role_type: RoleType, role):
+        if role_type == RoleType.HELPER:
+            self.helper_role = role
+        elif role_type == RoleType.MODERATOR:
+            self.moderator_role = role
+        else:
+            return True
+
+
+g_channels = Channels(None, None, None, None)
+g_roles = Roles(None, None)
